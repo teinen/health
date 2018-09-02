80a44f7b02a67995160db4bc927e32e7aaae2d2f
諦念 <teinen.github@gmail.com>
2018-09-02 21:52:01 +0900

    Add json import

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>Hello, world</h1>
+  <div>
+    <h1>Weight Data Graph</h1>
+    <div>
+      <p v-for="(value, key) in weights" :key="key">{{ key }}: {{ value }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import jsonData from './data/weight.json'
+
 export default {
-  
+  data () {
+    return {
+      weights: jsonData
+    }
+  },
 }
 </script>
-
-<style>
-
-</style>
