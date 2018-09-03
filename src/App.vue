8894a54a5e96965c3954c9051c132941ddf74374
諦念 <teinen.github@gmail.com>
2018-09-03 21:07:24 +0900

    Create data graph

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,18 @@ import jsonData from './data/weight.json'
 export default {
   data () {
     return {
-      weights: jsonData
+      periods: this.getPeriods(),
+      weights: this.getEachPeriodData()
+    }
+  },
+  methods: {
+    getPeriods (json) {
+      let keys = Object.keys(jsonData)
+      keys = keys.map(value => value.substr(0, 7))
+      return keys.filter((x, i, self) => self.indexOf(x) === i)
+    },
+    getEachPeriodData() {
+      
     }
   },
   mounted () {
@@ -19,17 +30,30 @@ export default {
     const myChart = new Chart(ctx, {
       type: 'line',
       data: {
+        // labels: ['2018-01-01', '2018-01-02','2018-01-03','2018-01-04','2018-01-05'],
         labels: Object.keys(jsonData),
         datasets: [{
             label: "Weights",
             lineTension: 0.2,
             backgroundColor: 'rgba(255, 99, 132, 0.3)',
             borderColor: 'rgba(255, 99, 132, 1)',
-            data: Object.values(jsonData),
+            // data: ['80.0', '79.5', '81.0', '81.5', '80.0'],
+            data: Object.values(jsonData)
         }]
       },
       options: {
-
+        scales: {
+          yAxes: [{
+            id: "y-axis",
+            type: "linear",
+            position: "left",
+            ticks: {
+                max: 85.0,
+                min: 75.0,
+                stepSize: 1.0
+            },
+          }]
+        }
       }
     })
   }
